36a891b7a6249ae855dac50a4e15ca139e361b59
AhmadK98 <52612156+AhmadK98@users.noreply.github.com>
2020-01-29 16:46:37 +0000

    imported front end

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  fetch('/api').then(res => res.json()).then(data => console.log(data))
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

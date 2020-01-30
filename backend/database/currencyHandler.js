63586c0f64f39f68f29c359283580dc9a00858aa
AhmadK98 <52612156+AhmadK98@users.noreply.github.com>
2020-01-30 16:35:01 +0000

    added wtd insert

--- backend/database/currencyHandler.js
+++ backend/database/currencyHandler.js
@@ -2,31 +2,31 @@ const fetch = require('node-fetch')
 const express = require('express');
 const { pool } = require('./pool')
 
-fetch('http://data.fixer.io/api/latest?access_key=27f41a0d85a9f902039854550b99758f&symbols=USD,AUD,CAD,GBP,PKR')
- .then(res => res.json())
- .then(data => Object.keys(data.rates).forEach((symbol) =>{
-     params = [symbol, data.rates[symbol]]
+// fetch('http://data.fixer.io/api/latest?access_key=27f41a0d85a9f902039854550b99758f&symbols=USD,AUD,CAD,GBP,PKR')
+//  .then(res => res.json())
+//  .then(data => Object.keys(data.rates).forEach((symbol) =>{
+//      params = [symbol, data.rates[symbol]]
      
-     try{
-        pool.query(`INSERT INTO currency (
-                symbol, 
-                gbp_convert, 
-                date)
-            VALUES($1,$2, current_date)`, params)
-     }catch(err){
-         console.log(err)
-     }
- })
- )
+//      try{
+//         pool.query(`INSERT INTO currency (
+//                 symbol, 
+//                 gbp_convert, 
+//                 date)
+//             VALUES($1,$2, current_date)`, params)
+//      }catch(err){
+//          console.log(err)
+//      }
+//  })
+//  )
 
-// const updateCurrency = async () => {
-//     const res = await fetch('https://api.exchangeratesapi.io/latest?base=GBP')
-//     const data = await res.json()
-//     Object.keys(await data.rates).forEach((symbol => {
-//         pool.query(`UPDATE currency SET gbp_convert = ${data.rates[symbol]} WHERE symbol = '${symbol}'`)
-//     }))
-// }
+const updateCurrency = async () => {
+    const res = await fetch('http://data.fixer.io/api/latest?access_key=27f41a0d85a9f902039854550b99758f&symbols=USD,AUD,CAD,GBP,PKR')
+    const data = await res.json()
+    Object.keys(await data.rates).forEach((symbol => {
+        pool.query(`UPDATE currency SET gbp_convert = ${data.rates[symbol]}, time = current_time WHERE symbol = '${symbol}'`)
+    }))
+}
 
-// updateCurrency()
+updateCurrency()
 
  
--- backend/database/stockHandler.js
+++ backend/database/stockHandler.js
@@ -51,7 +51,44 @@ const insertStock = async (ticker, name, fund, country) => {
     }
 }
 
-const updateStock = async (ticker, country) => { //add in aftermarket hours support
+
+const insertStockWtd = async (ticker, name, fund, country) => {
+    fund = fund || 'stock'
+    country = country || 'USA'
+
+    try {
+        if (country == 'UK') {
+            // ticker = ticker + '.L'
+            
+            stock = (await apiGetters.wtdAPI(ticker))[0]
+            console.log(stock)
+
+        } else {
+            
+            stock = (await apiGetters.wtdAPI(ticker))[0]
+            
+        }
+        let dataValue = `'[]'::jsonb || jsonb_build_object('time',to_char(current_timestamp, 'YYYY-MM-DD HH24:MI:SS'),'price',${await stock.price})`
+        let params = [await stock.symbol, await stock.name, fund, country, await stock.price, await stock.currency]
+        console.log(params)
+        let uploaded = await pgQuery(`INSERT INTO stocks (
+                                    ticker,
+                                    name,
+                                    fund,
+                                    country,
+                                    data, 
+                                    price,
+                                    currency) 
+                                VALUES (upper($1),$2,$3,$4,${await dataValue},$5, $6)`, params)
+                                
+         //add in response if price returns null values
+    } catch (err) {
+        console.log(err)
+    }
+}
+insertStockWtd(['VUSA.L'],'Vanguard',null,'UK')
+
+const updateStock = async (ticker, country) => { //only updates US stocks ATM
 
     country = country || 'USA'
     try {
@@ -117,22 +154,22 @@ const getCurrentValue = async (ticker, includeTime) => {  // gets latest object 
         }}
 }
 
-const updateAll = async () => {
+// const updateAll = async () => {
 
-    try {
-        const response = await pgQuery('SELECT ticker, country FROM stocks')
-        await response.rows.forEach(async ticker => {
-            try {
-                await updateStock(ticker.ticker, ticker.country)
-            } catch (err) {
-                console.log(err)
-            }
-        })
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+//     try {
+//         const response = await pgQuery('SELECT ticker, country FROM stocks')
+//         await response.rows.forEach(async ticker => {
+//             try {
+//                 await updateStock(ticker.ticker, ticker.country)
+//             } catch (err) {
+//                 console.log(err)
+//             }
+//         })
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
 
 const updateAllWtd = async () => {
     tickers = []

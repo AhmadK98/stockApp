78ff7bb185e2402bce777c9b949a75bd815dcdb5
Ahmad <kjahmadms2@gmail.com>
2020-02-02 14:53:09 +0000

    updated package.json

--- server.js
+++ server.js
@@ -27,13 +27,16 @@ app.get('/', (req, res) => {
     res.send('hiv')
 })
 
-port = process.env.PORT || 3001
+
 if (process.env.NODE_ENV ==='production'){
+    
     app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
+port = process.env.PORT || 3001
 app.listen(port, (res) => {
+    console.log(process.env.NODE_ENV)
     console.log(`connected on port ${port}`)
 })

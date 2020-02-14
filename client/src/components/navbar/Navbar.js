1f6719752a2906c0430a69861ade17c565a23eda
AhmadK98 <52612156+AhmadK98@users.noreply.github.com>
2020-02-14 00:02:48 +0000

    front end tweaking

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React
-// { useEffect, useState } 
-from 'react';
+    // { useEffect, useState } 
+    from 'react';
 import './navbar.scss';
 import {
     BrowserRouter as Router,
@@ -8,34 +8,25 @@ import {
     Route,
     NavLink,
     useHistory
-  } from "react-router-dom";
+} from "react-router-dom";
 
-  import StockGetter from '../stocks/StockGetter'
+import StockGetter from '../stocks/StockGetter'
 
 
 
 function Navbar() {
-    
+
 
     return (
 
         <div className="navbar">
+
             <div className="container">
-           
-                 
-                    <NavLink to="/">
-                        
-                        <div className="home" style={{"display": "block", "background":'blue',"height":'100%'}}>Home</div>
-                        
-                    </NavLink>
-                
-                <div className="dashboard">
-                    <NavLink to="/dash">Dashboard</NavLink>
-                </div>
-                <div className="login">
-                    <NavLink to="/login">Login</NavLink>
-                </div>
-    
+                <NavLink to="/" className='home'>stockTracker</NavLink>
+                <NavLink to="/dash" className='dashboard'>Dashboard</NavLink>
+                <NavLink to="/login" className='login'>Login</NavLink>
+
+
             </div>
 
         </div>
